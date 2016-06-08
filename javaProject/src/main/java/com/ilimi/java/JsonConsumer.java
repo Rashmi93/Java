80f8c75d407d76b108fed09c1dc8dfc5a615a8d1
Rashmi93 <rashminrocks93@gmail.com>
2016-06-08 15:05:25 +0530

    Update JsonConsumer.java

--- javaProject/src/main/java/com/ilimi/java/JsonConsumer.java
+++ javaProject/src/main/java/com/ilimi/java/JsonConsumer.java
@@ -1,17 +1,17 @@
 package com.ilimi.java;
-	
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
-import java.util.function.Consumer;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 // Consumer Class in Java
 @SuppressWarnings("rawtypes")
-public class JsonConsumer implements Runnable, Consumer{
+public class JsonConsumer implements Runnable, Consumer {
 
 	private BlockingQueue<ArrayList<String>> sharedQueue;
 	int consumer;
@@ -23,51 +23,56 @@ public class JsonConsumer implements Runnable, Consumer{
 	}
 
 	public void run() {
-		while(true){
-				try {
-					consume(consumer);
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (JsonParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (JsonMappingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		}	
-			
+		while (true) {
+			try {
+				consume(consumer);
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 	}
-	protected void consume(int consumer) throws InterruptedException, JsonParseException, JsonMappingException, IOException{
-		
-		ArrayList<String> eventData = sharedQueue.take();
-		
+
+	public void consume(int consumer) {
+
+		ArrayList<String> eventData = new ArrayList<String>();
+		try {
+			eventData = sharedQueue.take();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		System.out.println("Consumed by consumer: " + consumer + " :" + eventData);
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 		JsonPojo json = new JsonPojo();
-		
+
 		for (String event : eventData) {
-			json = mapper.readValue(event, JsonPojo.class);
+			try {
+				json = mapper.readValue(event, JsonPojo.class);
+			} catch (JsonParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			jsonObject.add(json);
 		}
 		System.out.println("jsonobject :" + jsonObject);
 		DbConnection con = new DbConnection();
-	    con.insertToDatabase(json);
-		
-	}
+		try {
+			con.insertToDatabase(json);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-	public void accept(Object t) {
-		// TODO Auto-generated method stub
-		
 	}
-
-	
 }
-
-	
